859afb653949f73845c39c3c13fcede76ec19321
protasovse <protasovse@gmail.com>
2017-12-13 15:41:48 +0500

    settings, rubric

--- project/apps/rubric/apps.py
+++ project/apps/rubric/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class RubricConfig(AppConfig):
     name = 'apps.rubric'
+    verbose_name = _('Рубрикатор')
+
