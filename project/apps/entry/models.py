2f7c18919b620b48a7bbfbdb042ae48980a68a93
protasovse <protasovse@gmail.com>
2017-12-17 18:01:34 +0500

    Модели файлов, лайков, предложений услуг. Исправлен.

--- project/apps/entry/models.py
+++ project/apps/entry/models.py
@@ -13,6 +13,16 @@ STATUS_CHOICES = ((DELETED, _('Удалённый')),
                   (DRAFT, _('Черновик')),
                   (PUBLISHED, _('Опубликован')))
 
+OFFER_NEW = '0'
+OFFER_VIEWED = '1'
+OFFER_PAID = '5'
+OFFER_CANCELED = '-1'
+
+OFFER_STATUS_CHOICES = ((OFFER_NEW, _('Новое')),
+                        (OFFER_VIEWED, _('Просмотрено клиентом')),
+                        (OFFER_PAID, _('Оплачен')),
+                        (OFFER_CANCELED, _('Отменён')))
+
 
 class Entry(models.Model):
     """
@@ -67,8 +77,8 @@ class Entry(models.Model):
         """
         return misaka.html(self.content)
 
-    class Meta:
-        abstract = True
+    # class Meta:
+    #     abstract = True
 
 
 class Files(models.Model):
@@ -167,7 +177,27 @@ class Offer(models.Model):
     """
     Предложение платных услуг юристом
     """
-    pass
+    answer = models.ForeignKey(Answer,
+                               on_delete=models.CASCADE)
+
+    cost = models.PositiveIntegerField(verbose_name=_('Стоимость услуг'))
+
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    # Оплачена ли услуга автором вопроса, на который дан ответ с предложениями
+    status = models.IntegerField(
+        _('Статус предложения'), db_index=True,
+        choices=OFFER_STATUS_CHOICES, default=OFFER_NEW)
+
+    paid_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Предложение услуг"
+        verbose_name_plural = "Предложения услуг"
+
+    def __str__(self):
+        return "%d. стоимость: %d₽. (ответ %d, вопрос %d)" % \
+               (self.pk, self.cost, self.answer.id, self.answer.on_question.id)
 
 
 def post_save_answer_receiver(sender, instance, *args, **kwargs):
