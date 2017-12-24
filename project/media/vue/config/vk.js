3bf88ac25f6c9947dd455f64748fcf6a7374ae18
ravall <ravall@MacBook-Air-Anna.local>
2017-12-24 11:14:42 +0300

    social auth

--- project/media/vue/config/vk.js
+++ project/media/vue/config/vk.js
@@ -1,8 +1,8 @@
 export default {
     VK_AUTHORIZE_URL:'https://oauth.vk.com/authorize',
-    VK_CLIENT_ID: 6278759,
+    VK_CLIENT_ID: 6302697,
     VK_DISPLAY: 'page',
-    VK_REDIRECT_URL: 'https://geodiscover.org/auth/vk',
+    VK_REDIRECT_URL: 'https://xn--h1abiilhh6g.xn--80asehdb/auth/vk',
     VK_RESPONCE_TYPE: 'code',
     VK_SCOPE: 4195331,
     VK_API_VERSION: 5.69
