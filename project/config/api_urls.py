5a89258eeaf7cfd85c479bdbf0df8812cabef5eb
ravall <ravall@MacBook-Air-Anna.local>
2018-03-09 08:33:15 +0300

    question

--- project/config/api_urls.py
+++ project/config/api_urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from apps.svem_auth.views import api
+from apps.rubric import view_api as rubric_api
+from apps.question.views import api as question_api
 
 urlpatterns = [
     path("user/check", api.CheckLogin.as_view()),
@@ -9,5 +11,7 @@ urlpatterns = [
     path("user/activate", api.ActivateAccount.as_view()),
     path("user/resend", api.ReSend.as_view()),
     path("user", api.AppUser.as_view()),
+    path('rubric', rubric_api.Rubrics.as_view()),
+    path('question', question_api.Question.as_view())
 ]
 
--- project/media/vue/routes/ask_question/ask/index.js
+++ project/media/vue/routes/ask_question/ask/index.js
@@ -1,27 +1,92 @@
 import template from './template.html';
 import form_mixin from '../../../mixins/form';
 
+
 export default {
+    mixins: [form_mixin],
     name: 'ask_question',
     data() {
         return {
-            options: ['foo', 'bar', 'baz']
+            rubric: [],
+            options: [],
+            base_options: [],
+            files: [],
+            id: 0,
+            is_authorized: false
         }
+    },
+    computed: {
+        post_action() {
+            return '/api/question/' + this.id
+        },
+    },
+    mounted() {
+        this.$http.get('/api/rubric', {params: {'level':0}},).then(
+            (r) => {
+                this.base_options = r.data.data;
+                this.optionsInit()
+            },
+        );
+        this.$http.get('/api/user/check').then(r=>{
+            this.is_authorized = r.data.success;
+        });
     },
     methods: {
         onSearch(search, loading) {
-            loading(true);
-            this.search(loading, search, this);
+            this.$http.get('/api/rubric', {params: {'keyword':search}}).then(
+                (r) => {
+                    this.options = r.data.data;
+                },
+            );
         },
-        search: _.debounce((loading, search, vm) => {
-      fetch(
-        `https://api.github.com/search/repositories?q=${escape(search)}`
-      ).then(res => {
-        res.json().then(json => (vm.options = json.items));
-        loading(false);
-      });
-    }, 350)
-  }
-},
+        optionsInit()
+        {
+            this.options = this.base_options;
+        },
+
+        save() {
+            try {
+                this.put('/api/question', {rubric: this.rubric}, () => {
+                    for (let i = 0; i < this.$refs.upload.files.length; i++) {
+                        this.$refs.upload.files[i].data = {id:3};
+                    }
+                    this.$refs.upload.active = true
+                });
+
+            } catch (err) {
+                this.set_form_error(err.message)
+            }
+        },
+        inputFilter(newFile, oldFile, prevent) {
+          if (newFile && !oldFile) {
+            // Before adding a file
+            // 添加文件前
+            // Filter system files or hide files
+            // 过滤系统文件 和隐藏文件
+            if (/(\/|^)(Thumbs\.db|desktop\.ini|\..+)$/.test(newFile.name)) {
+              return prevent()
+            }
+            // Filter php html js file
+            // 过滤 php html js 文件
+            if (/\.(php5?|html?|jsx?)$/i.test(newFile.name)) {
+              return prevent()
+            }
+          }
+        },
+        inputFile(newFile, oldFile) {
+          if (newFile && !oldFile) {
+            // add
+            console.log('add', newFile)
+          }
+          if (newFile && oldFile) {
+            // update
+            console.log('update', newFile)
+          }
+          if (!newFile && oldFile) {
+            // remove
+            console.log('remove', oldFile)
+          }
+        },
+    },
     template: template,
 }
