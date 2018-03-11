0494c90203a7c9a89f56a9e5b14aeab46d6c2a65
protasovse <protasovse@gmail.com>
2018-03-11 11:02:39 +0500

    Перенёл Review&Like в новое приложение

--- project/apps/entry/models.py
+++ project/apps/entry/models.py
@@ -102,51 +102,6 @@ class Files(models.Model):
     def __str__(self):
         return str(self.get_basename())
 
-'''
-class Likes(models.Model):
-    """
-    Лайки
-    """
-    entry = models.ForeignKey(Entry, on_delete=models.CASCADE, related_name='likes',
-                              verbose_name=_('Ответ'))
-
-    user = models.ForeignKey(AUTH_USER_MODEL, verbose_name=_('Пользователь'),
-                             help_text=_('Пользователь, который поставил отметку'), on_delete=models.CASCADE)
-
-    date = models.DateTimeField(_('Дата'), default=timezone.now)
-
-    value = models.SmallIntegerField(_('Балл'))
-
-    class Meta:
-        unique_together = ('entry', 'user')
-        ordering = ('-date',)
-        verbose_name = _('Отзыв')
-        verbose_name_plural = _('Отзывы')
-
-    @property
-    def title_for_admin(self):
-        return "Вопрос: %d" % (self.entry.answer.on_question_id)
-
-    def __str__(self):
-        return "%d: %s (%d)" % (self.entry.pk, self.user.get_full_name, self.value)
-
-
-class Review(models.Model):
-    """
-    Отзывы и комментарии к лайкам
-    """
-    like = models.OneToOneField(Likes, on_delete=models.CASCADE, related_name='review')
-    review = models.TextField(_('Текст отзыва'))
-
-    class Meta:
-        verbose_name = _('Отзыв')
-        verbose_name_plural = _('Отзывы')
-
-    def __str__(self):
-        return self.review
-
-'''
-
 
 class Titled(models.Model):
     """
