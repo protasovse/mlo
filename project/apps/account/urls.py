311842a58d0a969b60a1be8619f9dea0f7c126a6
protasovse <protasovse@gmail.com>
2018-03-15 17:22:20 +0500

    Редактирование профиля, образование, опыт работы, изменения

--- project/apps/account/urls.py
+++ project/apps/account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('редактировать-профиль/', AccountDetail.as_view(template_name='account/edit/main.html'), name='edit'),
     path('о-себе/', AccountDetail.as_view(template_name='account/edit/about.html'), name='edit_about'),
     path('образование/', AccountDetail.as_view(template_name='account/edit/education.html'), name='edit_education'),
+    path('опыт-работы/', AccountDetail.as_view(template_name='account/edit/experience.html'), name='edit_experience'),
 ]
