cb4c1f83cb25821f12e69d47413905ffe429f7eb
protasovse <protasovse@gmail.com>
2018-04-19 11:41:43 +0500

    delete_nonexistent_photos

---
+++ project/apps/account/utils.py
@@ -0,0 +1,17 @@
+from apps.account.models import Info
+
+
+def delete_nonexistent_photos():
+    """
+    Удаляем из базы фото, файлов который нет
+    :return:
+    """
+    info = Info.objects.all()
+    for i in info:
+        try:
+            print(i.orig.file)
+        except:
+            i.orig = None
+            i.pic = None
+            i.photo = None
+            i.save()
