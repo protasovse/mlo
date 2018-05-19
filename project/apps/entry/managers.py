7837efc72efde1461a16e1ae99c6d8d19a33e68a
protasovse <protasovse@gmail.com>
2018-05-19 10:23:56 +0500

    additional fix in question delta index

--- project/apps/entry/managers.py
+++ project/apps/entry/managers.py
@@ -120,7 +120,7 @@ class QuestionsPublishedManager(EntryPublishedManager):
 
         result = client.Query(query, 'question, question_delta')
 
-        # print(result)
+        print(result)
 
         if not result:
             qs = self.get_queryset().none()
